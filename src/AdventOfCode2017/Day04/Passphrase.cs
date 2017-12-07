b22a493858a8771657620b955d9d2538163c0c55
Imran Merchant <imerchant@outlook.com>
2017-12-07 10:25:05 -0600

    Added Day04 puzzle 1 solution

---
+++ src/AdventOfCode2017/Day04/Passphrase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2017.Day04
+{
+    public class Passphrase
+    {
+        public string Phrase { get; }
+        public List<string> Words { get; }
+        public bool IsValid => new HashSet<string>(Words).Count == Words.Count;
+
+        public Passphrase(string phrase)
+        {
+            Phrase = phrase;
+            Words = Phrase.SplitOn(' ').ToList();
+        }
+    }
+}
---
+++ test/AdventOfCode2017.Tests/Solutions/Day04Solutions.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using AdventOfCode2017.Day04;
+using AdventOfCode2017.Inputs;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2017.Tests.Solutions
+{
+    public class Day04Solutions : TestBase
+    {
+        public Day04Solutions(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void Puzzle1_CountValidPassphrases()
+        {
+            var phrases = Input.Day04Parse(Input.Day04).Select(x => new Passphrase(x));
+
+            phrases.Count(x => x.IsValid).Should().Be(455);
+        }
+
+        private const string Puzzle1Example =
+@"aa bb cc dd ee
+aa bb cc dd aa
+aa bb cc dd aaa";
+
+        [Fact]
+        public void Puzzle1_ExamplesPass()
+        {
+            var phrases = Input.Day04Parse(Puzzle1Example).Select(x => new Passphrase(x));
+
+            phrases.Count(x => x.IsValid).Should().Be(2);
+        }
+    }
+}
