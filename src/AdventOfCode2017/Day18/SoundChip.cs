3c31b85514491ce418c2528b461b8468c9557804
Imran Merchant <imerchant@outlook.com>
2017-12-23 20:32:15 -0600

    Added Day 18 puzzle 1 solution

---
+++ src/AdventOfCode2017/Day18/SoundChip.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2017.Day18
+{
+    public class SoundChip
+    {
+        public IDictionary<char, long> Registers { get; }
+
+        public SoundChip()
+        {
+            Registers = new Dictionary<char, long>();
+        }
+
+        public void FollowInstructions(IList<string> instructons)
+        {
+            long lastPlayedSound = 0;
+
+            for (var k = 0; k >= 0 && k < instructons.Count;)
+            {
+                var parts = instructons[k].SplitOn(' ');
+                var (id, value) = GetRegister(char.Parse(parts[1]));
+                if (parts[0] == "snd")
+                {
+                    if (value != 0)
+                        lastPlayedSound = value;
+                    ++k;
+                }
+                else if (parts[0] == "set")
+                {
+                    Registers[id] = GetValue(parts[2]);
+                    ++k;
+                }
+                else if (parts[0] == "add")
+                {
+                    Registers[id] += GetValue(parts[2]);
+                    ++k;
+                }
+                else if (parts[0] == "mul")
+                {
+                    Registers[id] *= GetValue(parts[2]);
+                    ++k;
+                }
+                else if (parts[0] == "mod")
+                {
+                    Registers[id] %= GetValue(parts[2]);
+                    ++k;
+                }
+                else if (parts[0] == "rcv")
+                {
+                    if (value != 0)
+                        throw new Exception($"recovered: {lastPlayedSound}");
+                    ++k;
+                }
+                else if (parts[0] == "jgz")
+                {
+                    if (value != 0)
+                        k += int.Parse(parts[2]);
+                    else
+                        ++k;
+                }
+                else
+                {
+                    throw new Exception("could not parse instruction");
+                }
+            }
+        }
+
+        private long GetValue(string part)
+        {
+            if (long.TryParse(part, out var value))
+            {
+                return value;
+            }
+
+            var (_, val) = GetRegister(char.Parse(part));
+            return val;
+        }
+
+        private (char id, long value) GetRegister(char id)
+        {
+            if (Registers.TryGetValue(id, out var value))
+            {
+                return (id, value);
+            }
+
+            value = 0;
+            Registers[id] = value;
+            return (id, value);
+        }
+    }
+}
---
+++ test/AdventOfCode2017.Tests/Solutions/Day18Solutions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using AdventOfCode2017.Day18;
+using AdventOfCode2017.Inputs;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2017.Tests.Solutions
+{
+    public class Day18Solutions : TestBase
+    {
+        public Day18Solutions(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void Puzzle1_FindFirstRecoveredValue()
+        {
+            var soundChip = new SoundChip();
+            var instructions = Input.Day18Parse(Input.Day18);
+
+            Action run = () => soundChip.FollowInstructions(instructions);
+
+            run.ShouldThrow<Exception>().WithMessage("recovered: 8600");
+        }
+
+        private const string Puzzle1Example =
+@"set a 1
+add a 2
+mul a a
+mod a 5
+snd a
+set a 0
+rcv a
+jgz a -1
+set a 1
+jgz a -2";
+
+        [Fact]
+        public void Puzzle1_ExamplePasses()
+        {
+            var soundChip = new SoundChip();
+            var instructions = Input.Day18Parse(Puzzle1Example);
+
+            Action run = () => soundChip.FollowInstructions(instructions);
+
+            run.ShouldThrow<Exception>().WithMessage("recovered: 4");
+        }
+    }
+}
