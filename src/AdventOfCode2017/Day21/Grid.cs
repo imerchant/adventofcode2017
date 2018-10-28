40bb39c0596b01b29d68cd78f3e45d7cf389348a
Imran Merchant <imerchant@outlook.com>
2018-10-28 15:55:05 -0500

    Added Day 21 puzzle 1 solution

--- src/AdventOfCode2017/Day21/Grid.cs
+++ src/AdventOfCode2017/Day21/Grid.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace AdventOfCode2017.Day21
 {
@@ -12,8 +14,21 @@ namespace AdventOfCode2017.Day21
             "###"
         };
 
-        public List<string> Content { get; }
+        public static List<Func<IEnumerable<string>, List<string>>> Transforms = new List<Func<IEnumerable<string>, List<string>>>
+        {
+            input => input.ToList(),
+            input => Rotate(input),
+            input => FlipHorizontal(FlipVertical(input)),
+            input => Rotate(FlipHorizontal(FlipVertical(input))),
+            input => FlipHorizontal(input),
+            input => Rotate(FlipHorizontal(input)),
+            input => FlipVertical(input),
+            input => Rotate(FlipVertical(input)),
+        };
+
+        public List<string> Content { get; private set; }
         public IDictionary<string, List<string>> Replacements { get; }
+        public int LitPixels => Content.Sum(x => x.Count(c => c == '#'));
 
         public Grid(IList<string> input)
         {
@@ -29,6 +44,112 @@ namespace AdventOfCode2017.Day21
                     split => split[0],
                     split => split[1].SplitOn('/').ToList()
                 );
+        }
+
+        internal static List<string> FlipHorizontal(IEnumerable<string> input)
+        {
+            return input.Select(x => x.Reverse().AsString()).ToList();
+        }
+
+        internal static List<string> FlipVertical(IEnumerable<string> input)
+        {
+            return input.Reverse().ToList();
+        }
+
+        internal static List<string> Rotate(IEnumerable<string> input)
+        {
+            var inputList = input.ToList();
+            var output = new List<string>();
+            for (var col = 0; col < inputList.Count; ++col)
+            {
+                var row = new StringBuilder();
+                for (var k = inputList.Count -1; k >= 0; --k)
+                {
+                    row.Append(inputList[k][col]);
+                }
+                output.Add(row.ToString());
+            }
+            return output;
+        }
+
+        private static string Flatten(IEnumerable<string> input)
+        {
+            return input.JoinStrings("/");
+        }
+
+        private List<string> FindReplacement(IEnumerable<string> input)
+        {
+            foreach (var transform in Transforms)
+            {
+                var result = transform(input);
+                if (Replacements.TryGetValue(Flatten(result), out var replacement))
+                {
+                    return replacement;
+                }
+            }
+            throw new Exception($"Could not locate replacement for {input.JoinStrings("/")}");
+        }
+
+        internal static List<string> Collapse(List<string>[][] input)
+        {
+            var output = new List<string>();
+            foreach (var splitRow in input)
+            {
+                var rowOutput = Enumerable.Repeat("", splitRow[0].Count).ToList();
+                foreach (var block in splitRow)
+                {
+                    for (var row = 0; row < block.Count; ++row)
+                    {
+                        rowOutput[row] += block[row];
+                    }
+                }
+                output.AddRange(rowOutput);
+            }
+            return output;
+        }
+
+        internal List<string> SplitAndReplace(int cardinality, List<string> input)
+        {
+            var splitCount = input.Count / cardinality;
+            var splitContent = new List<string>[splitCount][];
+            for (var k = 0; k < splitContent.Length; ++k)
+                splitContent[k] = new List<string>[splitCount];
+
+            for (var splitRow = 0; splitRow < splitCount; ++splitRow)
+            {
+                for (var splitCol = 0; splitCol < splitCount; ++splitCol)
+                {
+                    var output = new List<string>();
+                    for (var row = splitRow * cardinality; row < splitRow * cardinality + cardinality; ++row)
+                    {
+                        var rowOutput = new StringBuilder();
+                        for (var col = splitCol * cardinality; col < splitCol * cardinality + cardinality; ++col)
+                        {
+                            rowOutput.Append(input[row][col]);
+                        }
+                        output.Add(rowOutput.ToString());
+                    }
+                    splitContent[splitRow][splitCol] = FindReplacement(output);
+                }
+            }
+
+            return Collapse(splitContent);
+        }
+
+        public void Iterate()
+        {
+            if (Content.Count == 3)
+            {
+                Content = FindReplacement(Content);
+            }
+            else if(Content.Count % 2 == 0)
+            {
+                Content = SplitAndReplace(2, Content);
+            }
+            else if(Content.Count % 3 == 0)
+            {
+                Content = SplitAndReplace(3, Content);
+            }
         }
     }
 }
---
+++ test/AdventOfCode2017.Tests/Solutions/Day21Solutions.cs
@@ -0,0 +1,189 @@
+using System.Collections.Generic;
+using AdventOfCode2017.Day21;
+using AdventOfCode2017.Inputs;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2017.Tests.Solutions
+{
+    public class Day21Solutions : TestBase
+    {
+        public Day21Solutions(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void Puzzle1_CountLitPixels_After5Iterations()
+        {
+            var replacements = Input.Day21Parse(Input.Day21);
+            var grid = new Grid(replacements);
+
+            for (var k = 0; k < 5; ++k)
+            {
+                grid.Iterate();
+                // Output.WriteLine($"After iteration {k+1}:");
+                // Output.WriteLine($"{grid.Content.JoinStrings("\n")}");
+                // Output.WriteLine(string.Empty);
+            }
+
+            grid.LitPixels.Should().Be(110);
+        }
+
+        public const string Puzzle1Example =
+@"../.# => ##./#../...
+.#./..#/### => #..#/..../..../#..#";
+
+        [Fact]
+        public void Grid_IterateOnce_ExpandsContentCorrectly()
+        {
+            var replacements = Input.Day21Parse(Puzzle1Example);
+            var grid = new Grid(replacements);
+
+            grid.Iterate();
+
+            grid.Content.JoinStrings("/").Should().Be("#..#/..../..../#..#");
+        }
+
+        [Fact]
+        public void Grid_IterateTwice_ExpandsContentCorrectly()
+        {
+            var replacements = Input.Day21Parse(Puzzle1Example);
+            var grid = new Grid(replacements);
+
+            grid.Iterate();
+            grid.Iterate();
+
+            grid.Content.JoinStrings("/").Should().Be("##.##./#..#../....../##.##./#..#../......");
+            grid.LitPixels.Should().Be(12);
+        }
+
+        [Fact]
+        public void Grid_SplitAndReplace_SplitsAndReplaces4x4Correctly()
+        {
+            var content = new List<string>
+            {
+                "#..#",
+                "....",
+                "....",
+                "#..#"
+            };
+            var replacements = Input.Day21Parse(Puzzle1Example);
+            var grid = new Grid(replacements);
+
+            var output = grid.SplitAndReplace(2, content);
+
+            output.JoinStrings("/").Should().Be("##.##./#..#../....../##.##./#..#../......");
+        }
+
+        [Fact]
+        public void Grid_Splits2x2_Correctly()
+        {
+            var content = new List<string>
+            {
+                "#..#",
+                "....",
+                "....",
+                "#..#"
+            };
+
+            const int cardinality = 2;
+            var splitContent = new List<string>[2][];
+            for (var k = 0; k < splitContent.Length; ++k)
+                splitContent[k] = new List<string>[2];
+
+            for (var splitRow = 0; splitRow < 2; ++splitRow)
+            {
+                for (var splitCol = 0; splitCol < 2; ++splitCol)
+                {
+                    var output = new List<string>();
+                    for (var row = splitRow * cardinality; row < splitRow * cardinality + cardinality; ++row)
+                    {
+                        var rowOutput = "";
+                        for (var col = splitCol * cardinality; col < splitCol * cardinality + cardinality; ++col)
+                        {
+                            rowOutput += content[row][col];
+                        }
+                        output.Add(rowOutput);
+                    }
+                    splitContent[splitRow][splitCol] = output;
+                }
+            }
+
+            splitContent[0][0].JoinStrings("/").Should().Be("#./..");
+        }
+
+        [Fact]
+        public void Grid_Collapse2x2_OperatesCorrectly()
+        {
+            var input = new List<string>[][]
+            {
+                new List<string>[] { new List<string> { "#.", ".." }, new List<string>{ ".#", ".." } },
+                new List<string>[] { new List<string> { "..", "#." }, new List<string>{ "..", ".#" } }
+            };
+
+            var collapsed = Grid.Collapse(input);
+
+            collapsed.JoinStrings("/").Should().Be("#..#/..../..../#..#");
+        }
+
+        [Fact]
+        public void Grid_Collapse3x3_OperatesCorrectly()
+        {
+            var input = new List<string>[][]
+            {
+                new List<string>[] { new List<string> { "##.", "#..", "..." }, new List<string>{ "##.", "#..", "..." } },
+                new List<string>[] { new List<string> { "##.", "#..", "..." }, new List<string>{ "##.", "#..", "..." } }
+            };
+
+            var collapsed = Grid.Collapse(input);
+
+            collapsed.JoinStrings("/").Should().Be("##.##./#..#../....../##.##./#..#../......");
+        }
+
+        [Fact]
+        public void Grid_Rotate_RotatesCorrectly()
+        {
+            var input = new List<string>()
+            {
+                "123",
+                "456",
+                "789"
+            };
+
+            var output = Grid.Rotate(input);
+
+            output.JoinStrings(".").Should().Be("741.852.963");
+        }
+
+        [Fact]
+        public void Grid_FlipHorizontal_FlipsCorrect()
+        {
+            var input = new List<string>()
+            {
+                "123",
+                "456",
+                "789"
+            };
+
+            var output = Grid.FlipHorizontal(input);
+
+            output.JoinStrings(".").Should().Be("321.654.987");
+        }
+
+        [Fact]
+        public void Grid_FlipVertical_FlipsCorrect()
+        {
+            var input = new List<string>()
+            {
+                "123",
+                "456",
+                "789"
+            };
+
+            var output = Grid.FlipVertical(input);
+
+            output.JoinStrings(".").Should().Be("789.456.123");
+        }
+    }
+}
